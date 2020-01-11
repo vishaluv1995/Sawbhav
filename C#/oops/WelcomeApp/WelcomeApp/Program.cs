8bc057ae33605d18cd5c1bc34e1f7776ca289784
Vishal Upadhyay <upadhyayv1995@gmail.com>
2020-01-10 23:46:21 -0800

    Program.cs

---
+++ C#/oops/WelcomeApp/WelcomeApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WelcomeApp
+{
+    class Program
+    {
+
+        static void Main(string[] arguments)
+        {
+            //for (int i = 0; i < args.Length; i++)
+            //{
+            //    Console.WriteLine("Hello" + " " + args[i].ToString());
+            //}
+            foreach (string item in arguments)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
