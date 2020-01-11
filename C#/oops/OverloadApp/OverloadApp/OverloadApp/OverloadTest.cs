f41bc7e0bfd90dfeaa620ea09a062c6c34a812a6
Vishal Upadhyay <upadhyayv1995@gmail.com>
2020-01-11 00:17:21 -0800

    OverloadTest.cs

---
+++ C#/oops/OverloadApp/OverloadApp/OverloadApp/OverloadTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadApp
+{
+    class OverloadTest
+    {
+        static void Main(string[] args)
+        {
+            double objdbl = 10.55;
+
+            Console.WriteLine("Hello");
+            Console.WriteLine(objdbl);
+            Console.WriteLine('#');
+            Console.WriteLine(10.555555f);
+            Console.WriteLine(true);
+            Console.ReadLine();
+        }
+    }
+}
