4d5506d13d0becf978c52c78b51f9db0bd86889c
Vishal Upadhyay <upadhyayv1995@gmail.com>
2020-01-17 10:26:28 -0800

    RectangleEnaca

---
+++ C#/oops/RectangleEncapsuleApp/RectangleEncapsuleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace RectangleEncapsuleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectanlge objsmall = new Rectanlge();
+            objsmall.SetHeight(-200);
+            objsmall.SetWidth(105);
+
+            PrintInfo(objsmall);
+
+            Console.WriteLine();
+
+            Rectanlge objBig = new Rectanlge();
+            objBig.SetHeight(8);
+            objBig.SetWidth(19873677);
+            PrintInfo(objBig);
+
+            Rectanlge objTemp = objBig;
+            Console.WriteLine(objTemp.GetWidth());
+            Console.WriteLine(objBig.GetWidth());
+
+            objTemp.SetWidth(90);
+            Console.WriteLine(objTemp.GetWidth());
+            Console.WriteLine(objBig.GetWidth());
+
+            Console.WriteLine(objTemp.GetHashCode());
+
+            objTemp = null;
+            Console.WriteLine(objBig.GetWidth());
+            
+            Console.ReadLine();
+        }
+
+        public static void PrintInfo(Rectanlge temp)
+        {
+
+            Console.WriteLine("Height=" + temp.GetHeight());
+            Console.WriteLine("Widht=" + temp.GetWidth());
+            Console.WriteLine("Area " + temp.CalculteArea());
+            Console.WriteLine(temp.GetHashCode());
+
+        }
+    }
+}
---
+++ C#/oops/RectangleEncapsuleApp/RectangleEncapsuleApp/Rectanlge.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleEncapsuleApp
+{
+    class Rectanlge
+    {
+        private int width;
+        private int height;
+        public int CalculteArea()
+        {
+            return width * height;
+        }
+        public void SetWidth(int pwidth)
+        {
+            if (pwidth <= -1)
+            {
+                width = 1;
+            }
+            else if (pwidth > 100)
+            {
+                width = 100;
+            }
+            else
+            {
+                width = pwidth;
+            }
+        }
+        public void SetHeight(int pheight)
+        {
+            if (pheight <= -1)
+            {
+                height = 1;
+            }
+            else if (pheight > 100)
+            {
+                height = 100;
+            }
+            else
+            {
+                height = pheight;
+            }
+        }
+        public int GetHeight()
+        {
+            return height;
+        }
+        public int GetWidth()
+        {
+            return width;
+        }
+    }
+
+}
